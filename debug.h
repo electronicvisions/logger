902cf063b55c8a01fddf6ed761ea6c2a7428d398
Daniel Bruederle <bruederl@localhost.(none)>
2010-02-26 17:16:17 +0100

    renamed class, added comments

--- debug.h
+++ debug.h
@@ -12,63 +12,65 @@ typedef unsigned int uint;
 
 //! Singleton implementation of Debugger class
 /*! Only one single instance of this class can be created by calling the public function instance(). 
-Every further call returns a reference to this one class. */
+Every further call returns a reference to this one class. The parameters loglevel and logfile can 
+not be changed after their first initialization. 
+Pre-defined logging levels are: fatal=0,verbose=5,debug=10. */
 
-class DebugSingleton
+class LoggerSingleton
 {
 private:
-    uint dbglevel;
-    std::string dbgfilename;
-    std::ofstream *dbgfile;
+    uint loglevel; 
+    std::string logfilename; 
+    std::ofstream *logfile;
     std::ostringstream dummy;
-    static DebugSingleton *dbg;
-    DebugSingleton(uint level, std::string filename)
+    static LoggerSingleton *log;
+    LoggerSingleton(uint level, std::string filename)
     {
-        dbgfile = 0;
-        dbglevel = level;
-        dbgfilename = filename;
+        logfile = 0;
+        loglevel = level;
+        logfilename = filename;
     }
-    DebugSingleton(DebugSingleton& ds)
+    LoggerSingleton(LoggerSingleton& ds)
     {
     }
     bool belowThresh(uint level)
     {
-        return (level <= dbglevel);
+        return (level <= loglevel);
     }
 
 public:
-    ~DebugSingleton()
+    ~LoggerSingleton()
     {
-        if(dbgfile)
+        if(logfile)
         {
-            if(dbgfile->is_open()) dbgfile->close();
-            delete dbgfile;
+            if(logfile->is_open()) logfile->close();
+            delete logfile;
         }
     }
     
     enum levels {fatal=0,verbose=5,debug=10,all=99999};
     
-    uint * getLevel() { return &dbglevel; }
+    uint * getLevel() { return &loglevel; }
     
-    std::string getFilename() { return dbgfilename; }
+    std::string getFilename() { return logfilename; }
     
     std::ostream & operator() (uint level=fatal)
     {
         if(belowThresh(level))
         {
-            if(!dbgfile)
+            if(!logfile)
             {
-                if(dbgfilename=="") return std::cout;
+                if(logfilename=="") return std::cout;
                 else
                 {
-                    dbgfile = new std::ofstream;
-                    dbgfile->open(dbgfilename.c_str(), std::ios::out);
-                    *dbgfile << "";
-                    dbgfile->close();
-                    dbgfile->open(dbgfilename.c_str(), std::ios::app);
+                    logfile = new std::ofstream;
+                    logfile->open(logfilename.c_str(), std::ios::out);
+                    *logfile << "";
+                    logfile->close();
+                    logfile->open(logfilename.c_str(), std::ios::app);
                 }
             }
-            return *dbgfile;
+            return *logfile;
         }
         else
         {
@@ -79,14 +81,18 @@ public:
         }
     }
     
-    static DebugSingleton& instance(uint level=fatal, std::string filename="")
+    /*! This is the only way to create an instance of a LoggerSingleton. Only the first call actually creates an instance, 
+    all further calls returns a reference to the one and only instance. */
+    static LoggerSingleton& instance(
+        uint level=fatal,         //! The logging threshold: every message with a level higher than this threshold will NOT be logged.
+        std::string filename=""   //! The logging file: If nothing or an empty string is passed, std::cout is the default target for all outputs.
+    )
     {
-        if(!dbg) dbg = new DebugSingleton(level, filename);
-        return *dbg;
+        if(!log) log = new LoggerSingleton(level, filename);
+        return *log;
     }
 };
 
-// Allocating and initializing DebugSingleton's
-// static data members.  The pointer is being
-// allocated - not the object inself.
-DebugSingleton *DebugSingleton::dbg = 0;
+// Allocating and initializing LoggerSingleton's static data member.  
+// The pointer is allocated - not the object inself.
+LoggerSingleton *LoggerSingleton::log = 0;
