9775aad893a0b8a57a0d4a58e4962e4f04d123b7
Sebastian Jeltsch <sjeltsch@cetares.kip.uni-heidelberg.de>
2010-03-01 14:15:48 +0100

    ADD: some missing operator<< functions

--- logger.h
+++ logger.h
@@ -102,21 +102,33 @@ public:
         }
     }
 
-	std::ostream& operator<< (std::string msg)
-	{
-		return (*this)(DEBUG) << msg;
-	}
+	inline std::ostream& operator<<(std::basic_string<char> val)    { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(std::ostringstream& val)        { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(long val)                       { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(unsigned long val)              { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(bool val)                       { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(short val)                      { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(unsigned short val)             { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(int val)                        { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(unsigned int val)               { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(long long val)                  { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(unsigned long long val)         { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(double val)                     { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(float val)                      { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(long double val)                { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(const char* val)                { return (*this)(DEBUG) << val; }
+	inline std::ostream& operator<<(const void* val)                { return (*this)(DEBUG) << val; }
 
-    /*! This is the only way to create an instance of a LoggerSingleton. Only the first call actually creates an instance, 
-    all further calls returns a reference to the one and only instance. */
-    static LoggerSingleton& instance(
-        size_t level=FATAL,         //! The logging threshold: every message with a level higher than this threshold will NOT be logged.
-        std::string filename=""   //! The logging file: If nothing or an empty string is passed, std::cout is the default target for all outputs.
-    )
-    {
-        if(!log_ptr) log_ptr = boost::shared_ptr<LoggerSingleton> (new LoggerSingleton(level, filename));
-        return *log_ptr;
-    }
+	/*! This is the only way to create an instance of a LoggerSingleton. Only the first call actually creates an instance, 
+	  all further calls returns a reference to the one and only instance. */
+	static LoggerSingleton& instance(
+			size_t level=FATAL,         //! The logging threshold: every message with a level higher than this threshold will NOT be logged.
+			std::string filename=""     //! The logging file: If nothing or an empty string is passed, std::cout is the default target for all outputs.
+			)
+	{
+		if(!log_ptr) log_ptr = boost::shared_ptr<LoggerSingleton> (new LoggerSingleton(level, filename));
+		return *log_ptr;
+	}
 };
 
 #ifndef PYTHONEXPOSED
