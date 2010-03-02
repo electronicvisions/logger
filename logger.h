1b5c5a852d5ff72cda7e1b0782f48dbe03c89aae
Sebastian Jeltsch <sjeltsch@cetares.kip.uni-heidelberg.de>
2010-03-02 16:05:19 +0100

    ADD: Level-Tag and timestamp

--- logger.h
+++ logger.h
@@ -17,124 +17,214 @@
 
 //! Singleton implementation of Debugger class
 /*! Only one single instance of this class can be created by calling the public function instance(). 
-Every further call returns a reference to this one class. The parameters loglevel and logfile can 
-not be changed after their first initialization. 
-Pre-defined logging levels are: FATAL=0,VERBOSE=5,DEBUG=10.
-Default log level ist DEBUG if you reference the the debugger wit no explicit log level */
+  Every further call returns a reference to this one class. The parameters loglevel and logfile can 
+  not be changed after their first initialization. 
+  Pre-defined logging levels are: FATAL=0,VERBOSE=5,DEBUG=10.
+  Default log level ist DEBUG if you reference the the debugger wit no explicit log level */
 
-class LoggerSingleton;
-class LoggerSingleton
+class Logger;
+class Logger
 {
-private:
-    size_t loglevel;
-    std::string logfilename;
-    std::fstream* logfile;
-	std::ostringstream deafStream;
-    static boost::shared_ptr<LoggerSingleton> log_ptr;
-    LoggerSingleton(size_t level, std::string filename)
-    {
-        logfile = 0;
-        loglevel = level;
-        logfilename = filename;
-    }
-    LoggerSingleton(LoggerSingleton&) {}
-    bool belowThresh(size_t level)
-    {
-        return level <= loglevel ? true : false;
-    }
+	private:
+		size_t loglevel;
+		static boost::shared_ptr<Logger> log_ptr;
+		std::string logfilename;
+		std::fstream* logfile;
+		std::ostringstream deafStream;
 
-public:
-    ~LoggerSingleton()
-    {
-        if(logfile)
-        {
-            if(logfile->is_open())
-			{
-				logfile->flush();
-				logfile->close();
-			}
-            delete logfile;
-        }
-    }
-    
-    enum levels {FATAL=0,VERBOSE=5,DEBUG=10,ALL=99999};
-    
-    size_t getLevel() { return loglevel; }
-    
-    //! tells if the passed logging level is below the logger's threshold
-    bool willBeLogged(unsigned int level)
-    {
-        return (level <= loglevel);
-    }
-    //! Identical to willBeLogged(), for backward compatibility. 
-    bool l(unsigned int level)
-    {
-        return willBeLogged(level);
-    }
-    std::string getFilename() { return logfilename; }
-    
-    std::ostream& operator() (size_t level=DEBUG)
-    {
-        if(willBeLogged(level))
-        {
-            if(!logfile)
-            {
-                if(logfilename=="") return std::cout;
-                else
-                {
-					// erase existing log file if necessary
-                    logfile = new std::fstream;
-                    logfile->open(logfilename.c_str(), (std::fstream::out | std::fstream::binary) );
-                    *logfile << "";
-                    logfile->close();
+		Logger(size_t level, std::string filename);
+		Logger(Logger&);
 
-					// open clean log file in append mode
-                    logfile->open(logfilename.c_str(), (std::fstream::app | std::fstream::out | std::fstream::binary) );
-                }
-            }
-            return *logfile;
-        }
-        else
-        {
-			// return deaf stream
-			deafStream.clear();
-            return deafStream;
-        }
-    }
+		bool belowThresh(size_t level);
+		std::string getTime();
+		std::string toString(size_t level);
 
-	inline std::ostream& operator<<(std::basic_string<char> val)    { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(std::ostringstream& val)        { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(long val)                       { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(unsigned long val)              { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(bool val)                       { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(short val)                      { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(unsigned short val)             { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(int val)                        { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(unsigned int val)               { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(long long val)                  { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(unsigned long long val)         { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(double val)                     { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(float val)                      { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(long double val)                { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(const char* val)                { return (*this)(DEBUG) << val; }
-	inline std::ostream& operator<<(const void* val)                { return (*this)(DEBUG) << val; }
+	public:
+		enum levels {ERROR=0, WARNING=1, INFO=2, DEBUG=3, DEBUG1=4, DEBUG2=5, DEBUG3=6};
 
-	/*! This is the only way to create an instance of a LoggerSingleton. Only the first call actually creates an instance, 
-	  all further calls returns a reference to the one and only instance. */
-	static LoggerSingleton& instance(
-			size_t level=FATAL,         //! The logging threshold: every message with a level higher than this threshold will NOT be logged.
-			std::string filename=""     //! The logging file: If nothing or an empty string is passed, std::cout is the default target for all outputs.
-			)
-	{
-		if(!log_ptr) log_ptr = boost::shared_ptr<LoggerSingleton> (new LoggerSingleton(level, filename));
-		return *log_ptr;
-	}
+		~Logger();
+
+		/*! This is the only way to create an instance of a Logger. Only the first call actually creates an instance, 
+		  all further calls returns a reference to the one and only instance. */
+		static Logger& instance(
+				size_t level=WARNING,       //! The logging threshold: every message with a level higher than this threshold will NOT be logged.
+				std::string filename=""     //! The logging file: If nothing or an empty string is passed, std::cout is the default target for all outputs.
+				);
+
+		size_t getLevel();
+		std::string getFilename();
+
+		//! tells if the passed logging level is below the logger's threshold
+		bool willBeLogged(unsigned int level);
+
+		std::ostream& operator() (size_t level=DEBUG);
+
+		std::ostream& operator<<(std::basic_string<char> val);
+		std::ostream& operator<<(std::ostringstream& val);
+		std::ostream& operator<<(long val);
+		std::ostream& operator<<(unsigned long val);
+		std::ostream& operator<<(bool val);
+		std::ostream& operator<<(short val);
+		std::ostream& operator<<(unsigned short val);
+		std::ostream& operator<<(int val);
+		std::ostream& operator<<(unsigned int val);
+		std::ostream& operator<<(long long val);
+		std::ostream& operator<<(unsigned long long val);
+		std::ostream& operator<<(double val);
+		std::ostream& operator<<(float val);
+		std::ostream& operator<<(long double val);
+		std::ostream& operator<<(const char* val);
+		std::ostream& operator<<(const void* val);
 };
 
+
+Logger::Logger(size_t level, std::string filename)
+{
+	logfile = 0;
+	loglevel = level;
+	logfilename = filename;
+}
+
+Logger::Logger(Logger&) {}
+
+bool Logger::belowThresh(size_t level)
+{
+	return level <= loglevel ? true : false;
+}
+
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+
+#include <windows.h>
+
+inline std::string Logger::getTime()
+{
+	const int MAX_LEN = 200;
+	char buffer[MAX_LEN];
+	if (GetTimeFormatA(LOCALE_USER_DEFAULT, 0, 0, 
+				"HH':'mm':'ss", buffer, MAX_LEN) == 0)
+		return "Error in NowTime()";
+
+	char result[100] = {0};
+	static DWORD first = GetTickCount();
+	std::sprintf(result, "%s.%03ld ", buffer, (long)(GetTickCount() - first) % 1000);
+	return result;
+}
+
+#else
+
+#include <sys/time.h>
+
+inline std::string Logger::getTime()
+{
+	char buffer[11];
+	time_t t;
+	time(&t);
+	tm r = {0};
+	strftime(buffer, sizeof(buffer), "%X", localtime_r(&t, &r));
+	struct timeval tv;
+	gettimeofday(&tv, 0);
+	char result[100] = {0};
+	std::sprintf(result, "%s.%03ld ", buffer, (long)tv.tv_usec / 1000);
+	return result;
+}
+
+#endif //WIN32
+
+inline std::string Logger::toString(size_t level)
+{
+	static const char* const buffer[] = {"ERROR:\t", "WARNING:\t", "INFO:\t", "DEBUG:\t", "DEBUG1:\t", "DEBUG2:\t", "DEBUG3:\t"};
+	return buffer[level];
+}
+
+Logger::~Logger()
+{
+	if(logfile)
+	{
+		if(logfile->is_open())
+		{
+			logfile->flush();
+			logfile->close();
+		}
+		delete logfile;
+	}
+}
+
+Logger::Logger& Logger::instance(
+		size_t level,             //! The logging threshold: every message with a level higher than this threshold will NOT be logged.
+		std::string filename      //! The logging file: If nothing or an empty string is passed, std::cout is the default target for all outputs.
+		)
+{
+	if(!log_ptr) log_ptr = boost::shared_ptr<Logger> (new Logger(level, filename));
+	return *log_ptr;
+}
+
+inline size_t Logger::getLevel()
+{
+	return loglevel;
+}
+
+inline std::string Logger::getFilename()
+{
+	return logfilename;
+}
+
+inline bool Logger::willBeLogged(unsigned int level)
+{
+	return (level <= loglevel);
+}
+
+std::ostream& Logger::operator() (size_t level)
+{
+	if(willBeLogged(level))
+	{
+		if(!logfile)
+		{
+			if(logfilename=="") return  std::cout << toString(level);
+			else
+			{
+				// erase existing log file if necessary
+				logfile = new std::fstream;
+				logfile->open(logfilename.c_str(), (std::fstream::out | std::fstream::binary) );
+				*logfile << "";
+				logfile->close();
+
+				// open clean log file in append mode
+				logfile->open(logfilename.c_str(), (std::fstream::app | std::fstream::out | std::fstream::binary) );
+			}
+		}
+		return (*logfile << getTime() << toString(level));
+	}
+	else
+	{
+		// return deaf stream
+		deafStream.clear();
+		return deafStream;
+	}
+}
+
+inline std::ostream& Logger::operator<<(std::basic_string<char> val)    { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(std::ostringstream& val)        { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(long val)                       { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(unsigned long val)              { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(bool val)                       { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(short val)                      { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(unsigned short val)             { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(int val)                        { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(unsigned int val)               { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(long long val)                  { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(unsigned long long val)         { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(double val)                     { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(float val)                      { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(long double val)                { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(const char* val)                { return (*this)() << val; }
+inline std::ostream& Logger::operator<<(const void* val)                { return (*this)() << val; }
+
+
 #ifndef PYTHONEXPOSED
-// Allocating and initializing LoggerSingleton's static data member.  
+// Allocating and initializing Logger's static data member.  
 // The smart pointer is allocated - not the object inself.
-boost::shared_ptr<LoggerSingleton> LoggerSingleton::log_ptr;
+boost::shared_ptr<Logger> Logger::log_ptr;
 #endif //PYTHONEXPOSED
 
 #endif // __LOGGER_H__
