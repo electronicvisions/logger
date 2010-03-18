45b6418e0d4b33379795f06f897eb010893cd7b5
Sebastian Jeltsch <sjeltsch@cetares.kip.uni-heidelberg.de>
2010-03-18 17:41:54 +0100

    UPDATE: usage example with FLUSH usage

--- usage_example/main.cpp
+++ usage_example/main.cpp
@@ -19,6 +19,15 @@ int main()
 	// multi-line messages are also possible
 	log(Logger::INFO) << "First line of multi-line message." << std::endl << "    Second line of multi-line message.\n";
 	log << "    Third line of multi-line message.";
+
+	// use flush if you want the logger to print to the file immediately
+    log(Logger::ERROR) << "This message is written immediately" << Logger::flush;
+
+	// the following line would cause an exception by design (exceptions can be handled)
+    //log << "You can not write to an flushed stream instance anymore";
+
+	// you need to get a new message instance by using the ()-operator
+    log(Logger::INFO) << "New log stream instance. This message works again";
 }
 
 
