7ab20bb1a67b74105ac3ee387889eb3fe50940a4
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-07-20 15:20:07 +0200

    clang-format logger.h
    Change-Id: I72bf37d24d010f57fd7238b42e4c568ba4fc388a

--- log4cxx/logger.h
+++ log4cxx/logger.h
@@ -1,17 +1,17 @@
 #pragma once
-#include <sstream>
 #include <cassert>
-#include <string>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 
-#include <boost/thread/tss.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/thread/tss.hpp>
 
-#include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
-#include <log4cxx/propertyconfigurator.h>
 #include <log4cxx/consoleappender.h>
 #include <log4cxx/fileappender.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/propertyconfigurator.h>
 
 #include "colorlayout.h"
 #include "logging_ctrl.h"
@@ -19,8 +19,8 @@
 #define LOGGER_DEFAULT_LEVEL Logger::WARNING
 
 namespace visionary_logger {
-std::string print_backtrace() __attribute__ ((unused));
-std::string make_syslog_prefix() __attribute__ ((unused));
+std::string print_backtrace() __attribute__((unused));
+std::string make_syslog_prefix() __attribute__((unused));
 /// Prints a message to syslog, a prefix with general information will be added
 /// to the message
 /// Note: Don't use it in signal handlers
@@ -33,24 +33,32 @@ void write_to_syslog(
     log4cxx::LevelPtr level,
     log4cxx::LoggerPtr logger,
     log4cxx::spi::LocationInfo loc) __attribute__((unused));
-}
+} // namespace visionary_logger
 
 /// logger macros that print an additional backtrace
-#define LOG4CXX_DEBUG_BACKTRACE(logger, message) { \
-	LOG4CXX_DEBUG(logger, message); \
-	LOG4CXX_DEBUG(logger, visionary_logger::print_backtrace()); }
+#define LOG4CXX_DEBUG_BACKTRACE(logger, message)                                                   \
+	{                                                                                              \
+		LOG4CXX_DEBUG(logger, message);                                                            \
+		LOG4CXX_DEBUG(logger, visionary_logger::print_backtrace());                                \
+	}
 
-#define LOG4CXX_TRACE_BACKTRACE(logger, message) { \
-	LOG4CXX_TRACE(logger, message); \
-	LOG4CXX_TRACE(logger, visionary_logger::print_backtrace()); }
+#define LOG4CXX_TRACE_BACKTRACE(logger, message)                                                   \
+	{                                                                                              \
+		LOG4CXX_TRACE(logger, message);                                                            \
+		LOG4CXX_TRACE(logger, visionary_logger::print_backtrace());                                \
+	}
 
-#define LOG4CXX_INFO_BACKTRACE(logger, message) { \
-	LOG4CXX_INFO(logger, message); \
-	LOG4CXX_INFO(logger, visionary_logger::print_backtrace()); }
+#define LOG4CXX_INFO_BACKTRACE(logger, message)                                                    \
+	{                                                                                              \
+		LOG4CXX_INFO(logger, message);                                                             \
+		LOG4CXX_INFO(logger, visionary_logger::print_backtrace());                                 \
+	}
 
-#define LOG4CXX_WARN_BACKTRACE(logger, message) { \
-	LOG4CXX_WARN(logger, message); \
-	LOG4CXX_WARN(logger, visionary_logger::print_backtrace()); }
+#define LOG4CXX_WARN_BACKTRACE(logger, message)                                                    \
+	{                                                                                              \
+		LOG4CXX_WARN(logger, message);                                                             \
+		LOG4CXX_WARN(logger, visionary_logger::print_backtrace());                                 \
+	}
 
 // same same
 #define LOG4CXX_ERROR_BACKTRACE(logger, message) LOG4CXX_ERROR(logger, message)
@@ -94,24 +102,22 @@ void write_to_syslog(
 /// Get a log4cxx logger, while mimik the configuration behaviour of the old logger
 /// The logger will only be configure, if neither the root logger nor the
 /// Default logger has an appender
-log4cxx::LoggerPtr get_default_logger(std::string logger_name, log4cxx::LevelPtr level, std::string fname, bool dual);
+log4cxx::LoggerPtr get_default_logger(
+    std::string logger_name, log4cxx::LevelPtr level, std::string fname, bool dual);
 
-void configure_default_logger(log4cxx::LoggerPtr logger,
-		log4cxx::LevelPtr level, std::string fname, bool dual);
+void configure_default_logger(
+    log4cxx::LoggerPtr logger, log4cxx::LevelPtr level, std::string fname, bool dual);
 
 /// gets the "Default" instance from log4cxx
-inline
-log4cxx::Logger&
-get_log4cxx(
-	// don't change default argument. It's an empty "smart" pointer on purpose
-	log4cxx::LevelPtr level = log4cxx::LevelPtr(),
-	std::string logger_name = "Default",
-	std::string fname = std::string(),
-	bool dual = false)
+inline log4cxx::Logger& get_log4cxx(
+    // don't change default argument. It's an empty "smart" pointer on purpose
+    log4cxx::LevelPtr level = log4cxx::LevelPtr(),
+    std::string logger_name = "Default",
+    std::string fname = std::string(),
+    bool dual = false)
 {
 	static log4cxx::Logger* _logger;
-	if (!_logger)
-	{
+	if (!_logger) {
 		// never ever touch the allmighty &* ;)
 		//   http://osdir.com/ml/apache.logging.log4cxx.devel/2004-11/msg00028.html
 		_logger = &*get_default_logger(logger_name, level, fname, dual);
@@ -121,11 +127,16 @@ get_log4cxx(
 
 struct Message
 {
-	std::ostringstream& get() { return _stream; }
-	log4cxx::LevelPtr level() const { return _level; }
+	std::ostringstream& get()
+	{
+		return _stream;
+	}
+	log4cxx::LevelPtr level() const
+	{
+		return _level;
+	}
 
-	Message(log4cxx::LevelPtr level) :
-		_level(level), _stream() {}
+	Message(log4cxx::LevelPtr level) : _level(level), _stream() {}
 
 	~Message()
 	{
@@ -146,12 +157,14 @@ private:
 };
 
 
-struct NullStream :
-	public std::ostream
+struct NullStream : public std::ostream
 {
 	struct nullbuf : public std::streambuf
 	{
-		int overflow(int c) { return traits_type::not_eof(c); }
+		int overflow(int c)
+		{
+			return traits_type::not_eof(c);
+		}
 	} m_sbuf;
 
 	NullStream() : std::ios(&m_sbuf), std::ostream(&m_sbuf) {}
@@ -167,18 +180,13 @@ private:
 	size_t _last_level;
 
 	Logger(size_t level = LOGGER_DEFAULT_LEVEL) :
-		_buffer(&Message::custom_cleanup),
-		_null(),
-		_level(level),
-		_last_level(level)
+	    _buffer(&Message::custom_cleanup), _null(), _level(level), _last_level(level)
 	{}
 
 public:
-	static
-	log4cxx::LevelPtr log4cxx_level(size_t level)
+	static log4cxx::LevelPtr log4cxx_level(size_t level)
 	{
-		switch(level)
-		{
+		switch (level) {
 			case ERROR:
 				return log4cxx::Level::getError();
 			case WARNING:
@@ -194,10 +202,18 @@ public:
 		}
 	}
 
-	enum levels {ERROR=0, WARNING=1, INFO=2, DEBUG0=3, DEBUG1=4, DEBUG2=5, DEBUG3=6};
+	enum levels
+	{
+		ERROR = 0,
+		WARNING = 1,
+		INFO = 2,
+		DEBUG0 = 3,
+		DEBUG1 = 4,
+		DEBUG2 = 5,
+		DEBUG3 = 6
+	};
 
-	static
-	levels logger_level(log4cxx::LevelPtr level)
+	static levels logger_level(log4cxx::LevelPtr level)
 	{
 		if (level->isGreaterOrEqual(log4cxx::Level::getError()))
 			return ERROR;
@@ -212,12 +228,11 @@ public:
 		return DEBUG2;
 	}
 
-
 	static Logger& instance(
-		std::string logger_name = "Default",
-		size_t level = LOGGER_DEFAULT_LEVEL,
-		std::string file = "",
-		bool dual = false)
+	    std::string logger_name = "Default",
+	    size_t level = LOGGER_DEFAULT_LEVEL,
+	    std::string file = "",
+	    bool dual = false)
 	{
 		get_log4cxx(log4cxx_level(level), logger_name, file, dual);
 		static Logger _logger(level);
@@ -249,8 +264,7 @@ public:
 	}
 
 	//! Get stream instance
-	std::ostream&
-	operator() (size_t level)
+	std::ostream& operator()(size_t level)
 	{
 		_last_level = level;
 		if (_buffer.get() != NULL) {
@@ -267,8 +281,7 @@ public:
 	}
 
 	template <typename T>
-	std::ostream&
-	operator<<(T const& val)
+	std::ostream& operator<<(T const& val)
 	{
 		if (willBeLogged(_last_level))
 			return _buffer->get() << val;
@@ -283,7 +296,10 @@ public:
 	LoggerMixin() : mLog(Logger::instance()) {}
 	LoggerMixin(LoggerMixin const&) : mLog(Logger::instance()) {}
 
-	LoggerMixin& operator=(LoggerMixin const&) { return *this; }
+	LoggerMixin& operator=(LoggerMixin const&)
+	{
+		return *this;
+	}
 
 protected:
 	Logger& mLog;
